992101c86c6b5abce02ae2212d0e7b87d2e1d8c4
Danny Hernandez <dxherny@gmail.com>
2023-08-08 13:50:20 -0600

    pda

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from './ProfileCard'
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>Personal Digital Assistants</div>
+            <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImage} />
+            <ProfileCard title='Cortana' handle='@cortana32' image={CortanaImage} />
+            <ProfileCard title='Siri' handle='@siri01' image={SiriImage} />
+        </div>
+    );
 }
 
 export default App;
---
+++ pdas/src/ProfileCard.js
@@ -0,0 +1,13 @@
+function ProfileCard({title, handle, image }) {
+
+    return (
+    <div>
+        <img src={image} alt="pda logo" />
+        <div>This is {title}</div>
+        <div>Handle is {handle}</div>
+
+    </div>
+    );
+}
+
+export default ProfileCard;
